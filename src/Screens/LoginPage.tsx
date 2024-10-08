662f13c4f1a9d90f334f02f98b49e3a588d8b5ba
Devansh-ai <devansh.verma@appinventiv.com>
2024-10-08 10:36:46 +0530

    git configure check

---
+++ src/Screens/LoginPage.tsx
@@ -0,0 +1,33 @@
+// import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+// import React from 'react'
+
+// const LoginPage = () => {
+//    const  [name,setName]=useState('')
+//    const  [email,setEmail]=useState('')
+//    const  [password,setPassword]=useState('')
+//   return (
+//     <View>
+//       <TextInput
+//       placeholder='name'
+//       style={{borderWidth:1}}
+//       />
+//       <TextInput
+//       placeholder='email address'
+//       style={{borderWidth:1}}
+//       />
+//       <TextInput
+//       placeholder='password'
+//       style={{borderWidth:1}}
+//       />
+//       <TouchableOpacity style={{backgroundColor:'blue'}}>
+//         <Text>
+//             submit
+//         </Text>
+//       </TouchableOpacity>
+//     </View>
+//   )
+// }
+
+// export default LoginPage
+
+// const styles = StyleSheet.create({})
